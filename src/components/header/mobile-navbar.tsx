18666d88ee7f361e69e4bbc8ba1787c4ba3a4a3c
minhajul <dev.minhajul@gmail.com>
2024-09-25 19:36:45 +0600

    added zustand store and change the useState

--- src/components/header/mobile-navbar.tsx
+++ src/components/header/mobile-navbar.tsx
@@ -1,36 +1,30 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
 import { LINKS_TYPE } from "./navbar";
 import { Button } from "../ui/button";
 import { X, Menu } from "lucide-react";
+import { useMobileMenu } from "@/store";
 import { ModeToggle } from "./theme-toggle";
 import { usePathname } from "next/navigation";
 import { LanguageSwitcher } from "./language-switcher";
 
 const MobileNavbar = ({ links }: { links: LINKS_TYPE[] }) => {
   const path = usePathname();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { isOpen, onOpen, onClose } = useMobileMenu();
 
   return (
     <nav className="md:hidden">
       <section className="relative">
         {isOpen && (
-          <div
-            className="fixed inset-0 bg-black/50 z-40"
-            onClick={() => setIsOpen(false)}
-          />
+          <div className="fixed inset-0 bg-black/50 z-40" onClick={onClose} />
         )}
         <div
           className={`fixed top-0 left-0 w-72 sm:w-80 z-50 h-full duration-500  transition-transform ease-in-out bg-background text-foreground  ${
             isOpen ? "translate-x-0" : "-translate-x-full"
           }`}>
-          <div className="mb-4 px-6 pt-6 flex items-center justify-between">
-            <Button
-              onClick={() => setIsOpen(false)}
-              size="icon"
-              variant="outline">
+          <div className="mb-4 px-6 pt-6 flex items-center justify-end">
+            <Button onClick={onClose} size="icon" variant="outline">
               <X />
             </Button>
           </div>
@@ -59,10 +53,7 @@ const MobileNavbar = ({ links }: { links: LINKS_TYPE[] }) => {
       <div className="flex items-center gap-5">
         <LanguageSwitcher />
         <ModeToggle />
-        <Button
-          onClick={() => setIsOpen(!isOpen)}
-          size={"icon"}
-          variant="outline">
+        <Button onClick={onOpen} size={"icon"} variant="outline">
           <Menu />
         </Button>
       </div>
