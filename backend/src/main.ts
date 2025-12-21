8397449cd476a5fa0693be34b1c5b36af861c158
minhajul <dev.minhajul@gmail.com>
2025-12-21 22:10:24 +0600

    [CACHE] etag

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,8 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, If-None-Match',
+    exposedHeaders: ['ETag'],
   });
 
   app.useGlobalPipes(
